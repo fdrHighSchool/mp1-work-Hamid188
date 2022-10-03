4023f4af72373641ce3dc296fa18b2f8d5b2cc3f
AP Java Period 5 <apjavaperiod5@iphone.78b505.nycboe.org>
2022-10-03 12:08:08 -0400

    added fav movie files

---
+++ 01-favorite-movie/APJava5404.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class APJava5404 here.
+ *
+ * @author (Hamid)
+ * @version (9/22/22)
+ */
+public class APJava5404
+{
+    class Main {
+  public static void main(String[] args) {
+    favmovie();
+} // End of main method
+
+  public static void favmovie() { 
+    System.out.println("My favorite movie is Batman because of the action and suspense. ");
+ // End of favmovie method
+}
+

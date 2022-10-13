41ec90351516b54c2c6310808b33c08d8c0e68dd
AP Java Period 5 <apjavaperiod5@ac1mwk0jdjc9h.78b505.nycboe.org>
2022-10-13 12:09:38 -0400

    starter code

---
+++ 06-username/UserName.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class username here.
+ *
+ * @author (Hamid Ali)
+ * @version (10/12/2022)
+ */
+import java.util.Scanner;
+
+public class UserName {
+  public static void main(String[] args) {
+    // create Scanner object
+    Scanner s = new Scanner(System.in);
+
+    // get String input from user
+    System.out.print("Enter your first name: ");
+    String firstName = s.nextLine();
+    System.out.print("Enter your last name: ");
+    String lastName = s.nextLine();
+
+    // test output
+    System.out.println("Hello " + getInitial(firstName) + "." + getInitial(lastName) + ".");
+
+    s.close();
+  } // end main method
+
+  /*
+   * Name: getInitial
+   * Purpose: send back the first character (inital) of a name
+   * Input: a name (String)
+   * Return: a single character (String)
+   */
+  public static String getInitial(String n) {
+    return n.substring(0, 1);
+  } // end initialize method
+
+} // end class

5e563c3234cdefc0b878aefec0e56a4e07ba328f
AP Java Period 5 <apjavaperiod5@ac1mwk0jdjc9h.78b505.nycboe.org>
2022-10-24 12:04:45 -0400

    Username Updated

--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -8,16 +8,11 @@
 import java.util.Scanner;
 import java.util.Random;
 
+
 public class UserName {
   public static void main(String[] args) {
     // create a Scanner object
     Scanner s = new Scanner(System.in);
-    Random rand = new Random();
-    
-    int rand_int1 = rand.nextInt(10);
-    int rand_int2 = rand.nextInt(10);
-    int rand_int3 = rand.nextInt(10);
-    int rand_int4 = rand.nextInt(10);
     
     // Get A String Input From User
     System.out.print("Enter your first name: ");
@@ -25,21 +20,52 @@ public class UserName {
     System.out.print("Enter your last name: ");
     String lastName = s.nextLine();
     System.out.print("Enter a random number: ");
-    String favNum = s.nextLine();
+    String RandNum = s.nextLine();
     
-    // Get A String Input From User
-    System.out.print("Are you a teacher or a student?");
+    System.out.print("Are you a teacher or a student?:");
     String status = s.nextLine();
     status = status.toUpperCase();
 
 
-    if (status .equals("student")) {
-        System.out.println(firstName + lastName.substring(0, 1) + favNum + "@nycstudents.net");
+    if (status.equals("student")) {
+        System.out.println("Hello" + " " + firstName + lastName.substring(0, 1) + RandNum + "@nycstudents.net");
     }
-    if (status .equals("teacher")) {
-        System.out.println(firstName.substring(0, 1) + lastName + favNum + "@schools.nyc.gov");
+    if (status.equals("teacher")) {
+        System.out.println("Hello" + " " + firstName.substring(0, 1) + lastName + RandNum + "@schools.nyc.gov");
     }
-} 
+  } 
+  
+    
+  public static String generatePassword(int length) {
+    String password = "";
+
+    for(int i = 0; i < length; i++) {
+      // generate a random number
+      // 65 - 90 capital letter
+      // 97 - 122 lower case
+      // 33 - 47 special symbols
+      int capital = (char)(int)(Math.random()*(90-65+1) + 65);
+      int lowercase = (char)(int)(Math.random()*(122-97+1) + 95);
+      int specialsymbol = (char)(int)(Math.random()*(47-33+1) + 33);
+      
+      char C = (char)(capital);
+      char L = (char)(lowercase);
+      char S = (char)(specialsymbol);
+      
+      System.out.println("Your secret password is" + " " + capital + " " + C + lowercase + "" + L + specialsymbol + "" + S);
+      
+
+    } // End For Loop
+
+    return password;
+  } // End GeneratePassword Method
+}
+
+
+
+
+
+
 
 
 
